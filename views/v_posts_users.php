28d0cfbdfc2ba4a403650d7006dba1fce90d643c
Ed Hebert <ehebert@fas.harvard.edu>
2013-10-26 16:29:34 -0400

    styling changes to v_posts_users

--- views/v_posts_users.php
+++ views/v_posts_users.php
@@ -5,7 +5,7 @@
     <?php foreach($users as $blabbr): ?>
         <!-- you can't unfollow yourself so your button isn't shown -->
         <?php if ($user->user_id != $blabbr['user_id']) : ?>
-        <div class="col-sm-4 col-md-2">
+        <div class="col-xs-6 col-sm-3 col-md-2">
             <div class="thumbnail">
                 <!-- User info -->
                 <img class="img-circle avatar" src="/uploads/avatars/<?=$blabbr['image']?>" alt="<?=$blabbr['first_name']?> <?=$blabbr['last_name']?>">
